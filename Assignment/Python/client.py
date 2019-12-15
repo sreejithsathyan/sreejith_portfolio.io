a0fb676fe8241a39d18409d536e92b1345e3c9d2
sreejith <sreejithsathyan95@gmail.com>
2019-12-16 00:21:07 +0530

    ff

--- Assignment/Python/client.py
+++ Assignment/Python/client.py
@@ -1,8 +1,23 @@
 #Base Python client for MEng in IoT Assignment
 #consumes data from IoT Gateway
-import urllib2
+import matplotlib.pyplot as plt
 
-response = urllib2.urlopen('http://localhost:8080/')
-resp = response.read()
+import urllib.request 
+import json
+request_url = urllib.request.urlopen('http://localhost:8080/') 
+weatherData = json.loads(request_url.read()) 
+temp_data = []
+time_data = []
+for data in weatherData: 
+	temp_data.append(data.get("temperature")) 
+	time_data.append(data.get("time")) 
 
-print resp
+fig,ax=plt.subplots()
+ax.plot(time_data,temp_data,label='Weather forecast')
+y = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+ax.set_ylabel('°C')
+ax.set_title('Temperature')
+ax.legend()
+fig.autofmt_xdate()
+plt.show()
+
