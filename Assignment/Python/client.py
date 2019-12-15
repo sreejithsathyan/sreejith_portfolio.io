932cab307ba94b4f1e66ab8e0b258a94b3d4f6d5
sreejith <sreejithsathyan95@gmail.com>
2019-12-16 02:07:28 +0530

    python script update

--- Assignment/Python/client.py
+++ Assignment/Python/client.py
@@ -1,7 +1,6 @@
 #Base Python client for MEng in IoT Assignment
 #consumes data from IoT Gateway
 import matplotlib.pyplot as plt
-
 import urllib.request 
 import json
 request_url = urllib.request.urlopen('http://localhost:8080/') 
@@ -9,15 +8,14 @@ weatherData = json.loads(request_url.read())
 temp_data = []
 time_data = []
 for data in weatherData: 
-	temp_data.append(data.get("temperature")) 
+	temp = data.get("temperature")
+	temp_data.append(float(temp))
 	time_data.append(data.get("time")) 
-
 fig,ax=plt.subplots()
 ax.plot(time_data,temp_data,label='Weather forecast')
-y = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 ax.set_ylabel('°C')
-ax.set_title('Temperature')
+ax.set_xlabel('Time')
+ax.set_title('Temperature vs Time')
 ax.legend()
 fig.autofmt_xdate()
 plt.show()
-
